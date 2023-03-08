748814e5995971baddcda2c219813290d6052939
asadraiyan <asadraiyan001@gmail.com>
2023-03-08 17:21:44 +0530

    added increment & decrement functionality using redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 
 import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { incNumber, decNumber } from './actions';
 
 function App() {
+
+  const myState = useSelector((state) => state.changeNumber)
+  const dispatch = useDispatch()
   return (
     <div className="main-container">
       <div className="container">
         <h2>Increment/Decrement Counter</h2>
         <div className="counter">
-          <button className='increment'>+</button>
-          <input type="text" className='text' value={0} />
-          <button className='decrement'>-</button>
+          <button className='increment' onClick={() => dispatch(incNumber(5))}>+</button>
+          <input type="text" className='text' value={myState} />
+          <button className='decrement' onClick={() => dispatch(decNumber(3))}>-</button>
         </div>
         <h3>Using React and Redux</h3>
 
---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export const incNumber = (num) => {
+    return {
+        type: "Increment",
+        payload: num
+    }
+}
+export const decNumber = (num) => {
+    return {
+        type: "Decrement",
+        payload: num
+    }
+}
